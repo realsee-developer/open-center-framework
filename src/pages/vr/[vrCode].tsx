35654c1235df40c62c29caa04665773deb011364
sansi-lcj <lichengjie003@realsee.com>
2022-07-11 17:29:10 +0800

    feat: query参数支持

--- src/pages/vr/[vrCode].tsx
+++ src/pages/vr/[vrCode].tsx
@@ -1,32 +1,51 @@
-import { getRealseeSpaceProps, RealseeSpace } from '@/lib'
-import { RealseeSpaceProps } from '@/lib/core/typings/RealseeSpaceProps'
-import dynamic from 'next/dynamic'
-import Head from 'next/head'
-import { useCallback, useEffect, useState } from 'react'
+import { getRealseeSpaceProps, RealseeSpace } from "@/lib";
+import { RealseeSpaceProps } from "@/lib/core/typings/RealseeSpaceProps";
+import logger from "@/lib/core/utils/logger";
+import { parseQuery } from "@/lib/core/utils/qs";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import { useCallback, useEffect, useState } from "react";
 
-const ExampleApp = dynamic(() => import('@/apps/example'), { ssr: false })
+const ExampleApp = dynamic(() => import("@/apps/example"), { ssr: false });
 
 const useRealseeSpaceProps = () => {
-  const [realseeSpaceProps, setRealseeSpaceProps] = useState<RealseeSpaceProps | undefined>()
+  const [realseeSpaceProps, setRealseeSpaceProps] = useState<
+    RealseeSpaceProps | undefined
+  >();
 
   const initRealseeSpaceProps = useCallback(async () => {
     const { props } = await getRealseeSpaceProps({
       getConfig: (defaultConfig, originConfig) => {
-        return Object.assign({}, defaultConfig, originConfig)
+        logger.info("默认配置：", defaultConfig);
+        logger.info("远程配置：", originConfig);
+        const config = { ...defaultConfig, ...originConfig };
+        const query = parseQuery(globalThis.location.search);
+        const queryConfig = Object.keys(query)
+          .filter(
+            (key) =>
+              typeof query[key] === "string" &&
+              ["0", "1", "true", "false"].includes(query[key] as string)
+          )
+          .map((key) => ({
+            [key]: query[key] === "1" || query[key] === "true",
+          }))
+          .reduce((a, b) => ({ ...a, ...b }), {});
+        logger.info("参数配置：", queryConfig);
+        return { ...config, ...queryConfig };
       },
-    })
-    setRealseeSpaceProps(props)
-  }, [])
+    });
+    setRealseeSpaceProps(props);
+  }, []);
 
   useEffect(() => {
-    initRealseeSpaceProps()
-  }, [initRealseeSpaceProps])
+    initRealseeSpaceProps();
+  }, [initRealseeSpaceProps]);
 
-  return realseeSpaceProps
-}
+  return realseeSpaceProps;
+};
 
 const Space = () => {
-  const realseeSpaceProps = useRealseeSpaceProps()
+  const realseeSpaceProps = useRealseeSpaceProps();
   return (
     <>
       <Head>
@@ -34,7 +53,10 @@ const Space = () => {
         <meta charSet="UTF-8" />
         <link href="/favicon.ico" type="image/x-icon" rel="icon" />
         {/* viewport 必须设置好  */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
       </Head>
       {realseeSpaceProps && (
         <RealseeSpace {...realseeSpaceProps}>
@@ -42,7 +64,7 @@ const Space = () => {
         </RealseeSpace>
       )}
     </>
-  )
-}
+  );
+};
 
-export default Space
+export default Space;
